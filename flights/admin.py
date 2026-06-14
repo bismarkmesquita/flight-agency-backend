745da742f2de65b054cf822b5484df2e0f8c22b5
Bismark Mesquita do Nascimento <bismark1816@gmail.com>
2026-06-14 18:50:57 -0300

    feat(flights): add flights.admin

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from flights.models import Airline, Airport, Flight
 
-# Register your models here.
+
+class AirlineAdmin(admin.ModelAdmin):
+    list_display = ["name", "iata"]
+    search_fields = ["name", "iata"]
+    ordering = ["name"]
+
+
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ["iata", "icao", "name"]
+    search_fields = ["iata", "icao", "name"]
+
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ["iata", "airline", "departure_date", "arrival_date"]
+    list_filter = ["airline"]
+    search_fields = [
+        "iata",
+        "airline__name",
+        "departure_airport",
+        "arrival_airport"
+    ]
+    ordering = ["-departure_date"]
+
+
+admin.site.register(Airline, AirlineAdmin)
+admin.site.register(Airport, AirportAdmin)
+admin.site.register(Flight, FlightAdmin)
