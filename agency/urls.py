59e1e89543b7fb5f526c146c046c678d627e6dc1
Bismark Mesquita do Nascimento <bismark1816@gmail.com>
2026-06-16 12:15:12 -0300

    feat(agency): implement SalesView and tests

--- agency/urls.py
+++ agency/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from agency.views.customers import CustomerView, CustomersView
 from agency.views.reservation import ReservationsView
+from agency.views.sales import SalesView
 
 
 urlpatterns = [
     path("customers/", CustomersView.as_view()),
     path("customers/<int:id>/", CustomerView.as_view()),
     path("reservations/", ReservationsView.as_view()),
+    path("sales/", SalesView.as_view()),
 ]
